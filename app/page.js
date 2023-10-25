572289bf8d9ae8f1b688fd13ecdcde4fba6fea2e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-10-26 01:15:25 +0530

    Understansing React UseState

--- app/page.js
+++ app/page.js
@@ -1,13 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const page = () => {
-  let value = 1;
-  const changevalue = () => {
-    console.log(value);
-    value = value + 1;
-    console.log(value);
-  };
+  const [value, setValue] = useState(1);
   return (
     <>
       <h1 className="font-bold ml-5 mt-5">
@@ -15,7 +10,9 @@ const page = () => {
       </h1>
       <h1 className="ml-5">Watching this in iPad</h1>
       <button
-        onClick={changevalue}
+        onClick={() => {
+          setValue(99999);
+        }}
         className="bg-black font-bold text-white px-5 py-2 rounded mt-5 ml-5"
       >
         Update
